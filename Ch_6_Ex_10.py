130567a8c8b9e933b0b36883e0f8f890ccc14c49
tbuzzworthy <78036739+tbuzzworthy@users.noreply.github.com>
2021-02-01 09:14:59 -0500

    Add files via upload

---
+++ Ch_6_Ex_10.py
@@ -0,0 +1,10 @@
+"my answer"
+
+april = "It was a bright cold day in April, and the clocks were striking thirteen."
+print(april[0:33])
+
+"answer key"
+
+sentence = "It was a bright cold day in April, and the clocks were striking thirteen."
+slce = sentence[0:33]
+print(slce)
